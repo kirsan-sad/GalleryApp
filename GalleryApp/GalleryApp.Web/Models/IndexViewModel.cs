11dc70507be80bd7f5ab61ec31e2d9cc5e1b435e
Kirsan <webmasters@list.ru>
2021-04-03 11:22:21 +0300

    [+] add auth, add Automatic Image Montage with jQuery library

---
+++ GalleryApp/GalleryApp.Web/Models/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GalleryApp.Domain.Models;
+using GalleryApp.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GalleryApp.Web.Models
+{
+    public class IndexViewModel
+    {
+        public int GenreId { get; set; }
+        public string Genre { get; set; }
+        public List<PhotoEntity> Photos { get; set; }
+    }
+}
